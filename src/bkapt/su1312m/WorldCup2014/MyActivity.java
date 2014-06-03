679c21c151e23b3a50bcb35e4044f3d4eae0ef5d
RibboN <dangky_online1188@yahoo.com>
2014-06-03 22:33:37 +0700

    Init project

---
+++ src/bkapt/su1312m/WorldCup2014/MyActivity.java
@@ -0,0 +1,16 @@
+
+package bkapt.su1312m.WorldCup2014;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
