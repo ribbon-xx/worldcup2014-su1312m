acff1d8f3d55bce4c4c3a321cb9f9f6de3d5f74d
hyperion <hyperion@3i.com.vn>
2014-06-14 21:04:03 +0700

    edit getall matches

--- src/bkapt/su1312m/WorldCup2014/GlobalVariable.java
+++ src/bkapt/su1312m/WorldCup2014/GlobalVariable.java
@@ -7,5 +7,5 @@ public class GlobalVariable {
 	public static String Id_Number = "";
 	public static String Add = "";
 	public static String Work = "";
-	public static String URL = "http://cup.tin9x.vn/user/register1.php";
+	public static String URL = "http://cup.tin9x.vn/user/register2.php";
 }
